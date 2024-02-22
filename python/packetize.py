42eaa8d2dbf6c6b5e24ebdc19d5a6bc86b6c26ca
Clayton Smith <argilo@gmail.com>
2024-02-22 14:46:37 -0500

    Add support for type 2 packets

--- python/packetize.py
+++ python/packetize.py
@@ -28,16 +28,15 @@ from gnuradio import gr
 
 
 class packetize(gr.basic_block):
-    # The preamble (32 ones) and SFD (0000 1100 1011 1101) in Manchester form
-    preamble_sfd = numpy.array([1, 0]*32 + [0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0], dtype=numpy.int8).tostring()
+    preamble = numpy.repeat(numpy.array([1, 0]*16, dtype=numpy.uint8), 4).tobytes()
+    sfd_1 = numpy.repeat(numpy.array([0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0], dtype=numpy.uint8), 4).tobytes()
+    sfd_2 = numpy.repeat(numpy.array([1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0], dtype=numpy.uint8), 4).tobytes()
+    manchester = [int(f"{b:08b}"[:4].count("1") > f"{b:08b}"[4:].count("1")) for b in range(256)]
 
-    """
-    docstring for block packetize
-    """
     def __init__(self, num_inputs):
         gr.basic_block.__init__(self,
                                 name="packetize",
-                                in_sig=[numpy.int8]*num_inputs,
+                                in_sig=[numpy.uint8]*num_inputs,
                                 out_sig=None)
 
         i = 1
@@ -49,6 +48,10 @@ class packetize(gr.basic_block):
         self.linktype = 147
         self.file.write(struct.pack("IHHIIII", 0xa1b2c3d4, 2, 4, 0, 0, 32767, self.linktype))
         self.file.flush()
+
+        self.packet_bits = [numpy.array([], dtype=numpy.uint8) for _ in range(num_inputs)]
+        self.packet_type = [0] * num_inputs
+        self.bits_remaining = [0] * num_inputs
 
     def __del__(self):
         self.file.close()
@@ -69,13 +72,17 @@ class packetize(gr.basic_block):
         reg ^= 0xffff
         return bytes([reg & 0xff, reg >> 8])
 
-    def process_packet(self, channel, bits):
-        pkt = bytes(numpy.packbits(bits) ^ 0x55)
-        length = pkt[0]
-        if length + 2 > len(pkt):
+    def process_packet(self, channel, pkt):
+        if pkt[0] >= 2:
+            len_bytes = 1
+            length = pkt[0]
+        else:
+            len_bytes = 2
+            length = (pkt[0] << 8) | pkt[1]
+
+        if length + 2 != len(pkt):
             print("Invalid packet length.")
             return
-        pkt = pkt[0:length+2]
         if self.crc_x25(pkt[0:length]) != pkt[length:length+2]:
             print("Invalid checksum.")
             return
@@ -89,55 +96,90 @@ class packetize(gr.basic_block):
         self.file.write(pkt[0:-2])
         self.file.flush()
 
-        len1, flag1, src, dst = struct.unpack(">BBII", pkt[0:10])
+        payload = pkt[len_bytes:-2]
+        flag1, src, dst = struct.unpack(">BII", payload[0:9])
         time_str = datetime.datetime.now().strftime("%H:%M:%S.%f")
-        print(f"{time_str} {channel:02}  {len1:02x} {flag1:02x} {src:08x} {dst:08x} {pkt[10:13].hex()} {pkt[13:16].hex()} {pkt[16:-2].hex()} {pkt[-2:].hex()}")
+        print(f"{time_str} {channel:02}  {pkt[0:len_bytes].hex()} {flag1:02x} {src:08x} {dst:08x} {payload[9:12].hex()} {payload[12:15].hex()} {payload[15:].hex()} {pkt[-2:].hex()}")
 
         if src & 0x80000000 == 0 and len(pkt) > 16:
-            len4 = pkt[16]
-            if len4 == len1 - 17:  # 1st byte of payload is a length
+            len4 = payload[15]
+            if len4 == length - 17:  # 1st byte of payload is a length
                 if len(pkt) > 18:
-                    cmd = pkt[18]
+                    cmd = payload[17]
                     if cmd == 0xce and len(pkt) >= 64:  # hourly usage data, every 6 hours
                         print()
 
-                        main_reading = pkt[61:64].hex()
+                        main_reading = payload[60:63].hex()
                         print(f"  Meter reading for meter #{src}: {main_reading} kWh")
 
-                        n_hours = pkt[26]
+                        n_hours = payload[25]
                         if n_hours > 17:
                             print(f"  Number of hourly readings is too high: {n_hours}")
                             n_hours = 17
-                        hourly_readings = [reading / 100 for reading in struct.unpack(">" + "H"*n_hours, pkt[27:27 + 2*n_hours])]
+                        hourly_readings = [reading / 100 for reading in struct.unpack(">" + "H"*n_hours, payload[26:26 + 2*n_hours])]
                         readings_str = ", ".join(f"{reading:.2f}" for reading in hourly_readings)
                         print(f"  Hourly readings: {readings_str}")
 
                         print()
 
-    def manchester_demod_packet(self, channel, man_bits):
-        for offset in range(0, len(man_bits), 2):
-            if man_bits[offset] == man_bits[offset+1]:
-                # Manchester error. Discard packet.
-                break
-        else:
-            # We've got a valid packet! Throw out the preamble and SFD
-            # and extract the bits from the Manchester encoding.
-            self.process_packet(channel, man_bits[96::2])
-
     def forecast(self, noutput_items, ninputs):
-        return [2496] * ninputs
+        return [640] * ninputs
 
     def general_work(self, input_items, output_items):
-        # Wait until we get at least one packet worth of Manchester bits
-        if len(input_items[0]) < 1248:
-            return 0
-
         for channel, bits in enumerate(input_items):
-            bits_string = bits.tostring()
-            index = bits_string.find(self.preamble_sfd, 0, -1248+96)
-            while index != -1:
-                self.manchester_demod_packet(channel, bits[index:index+1248])
-                index = bits_string.find(self.preamble_sfd, index+1248, -1248+96)
-            self.consume(channel, len(bits)-1247)
+            if self.bits_remaining[channel] > 0:
+                bits_to_take = min(self.bits_remaining[channel], len(bits))
+                self.packet_bits[channel] = numpy.append(self.packet_bits[channel], bits[:bits_to_take])
+                self.bits_remaining[channel] -= bits_to_take
+                if self.bits_remaining[channel] == 0:
+                    if self.packet_type[channel] == 1:
+                        packet_manchester = numpy.packbits(self.packet_bits[channel])
+                        packet_bits = numpy.fromiter((self.manchester[b] for b in packet_manchester), dtype=numpy.uint8)
+                        packet = (numpy.packbits(packet_bits) ^ 0x55).tobytes()
+                        self.process_packet(channel, packet)
+                    if self.packet_type[channel] == 2:
+                        packet = (numpy.packbits(self.packet_bits[channel]) ^ 0xaa).tobytes()
+                        self.process_packet(channel, packet)
+                    self.packet_bits[channel] = numpy.array([], dtype=numpy.uint8)
+                self.consume(channel, bits_to_take)
+            else:
+                if len(bits) >= 320:
+                    bits_string = bits.tobytes()
+                    offset = 0
+                    while True:
+                        index = bits_string.find(self.preamble, offset, -192)
+                        if index == -1:
+                            offset = len(bits) - 319
+                            break
+                        else:
+                            offset = index
+
+                        if bits_string[offset+128:offset+256] == self.sfd_1:
+                            offset += 256
+                            length = 0
+                            for digit_offset in range(0, 64, 8):
+                                digit_bits = bits_string[offset + digit_offset:offset + digit_offset + 8]
+                                digit_byte = 0
+                                for digit_bit in digit_bits:
+                                    digit_byte = (digit_byte << 1) | digit_bit
+                                length = (length << 1) | self.manchester[digit_byte]
+                            if length != -1:
+                                length ^= 0x55
+                                self.bits_remaining[channel] = (length + 2) * 64
+                                self.packet_type[channel] = 1
+                                break
+                        elif bits_string[offset+128:offset+256] == self.sfd_2:
+                            offset += 256
+                            length = 0
+                            for bit in bits_string[offset:offset+16]:
+                                length = (length << 1) | bit
+                            length ^= 0xaaaa
+                            if length < 512:
+                                self.bits_remaining[channel] = (length + 2) * 8
+                                self.packet_type[channel] = 2
+                                break
+                        else:
+                            offset += 8
+                    self.consume(channel, offset)
 
         return 0
